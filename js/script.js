63888db4634d7d2428758d0a943288c5614fe1aa
lakroune <lakroune00@gmail.com>
2025-10-31 11:37:55 +0100

    create: function delete

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
-
+let my_key = "Baztami"
 
 let cardTransaction = document.getElementById("cardTransaction")
 let clescardTransaction = document.getElementById("clescardTransaction")
 let nouvelleTransaction = document.getElementById("nouvelleTransaction")
+let btnsauvgarder_modification = document.getElementById("sauvgarder_modification")
+let btnsave = document.getElementById("btnsave")
+let btnajout = document.getElementById("btnajout")
 let btnanullerTransaction = document.getElementById("btnanullerTransaction")
 let ligthmode = document.getElementById("ligthmode")
 let darkmode = document.getElementById("darkmode")
@@ -112,15 +115,27 @@ function get_Discription() {
 function get_Type() {
     return document.getElementById('type').value;
 }
+//set montant
+function set_Montant(montant) {
+    document.getElementById('montant').value = montant;
+}
+// set Discription de operation 
+function set_Discription(discription) {
+    document.getElementById('discription').value = discription;
+}
+// set type de operation Dépense ou Revenu
+function set_Type(type) {
+    document.getElementById('type').value = type;
+}
 
 // verifier le champ de input Mantant
 function verifier_Montant() {
     if (get_Montant() > 0) {
         document.getElementById('montant').classList.remove('bg-danger')
         return 1
-    }else {
-         document.getElementById('montant').classList.add('bg-danger')
-           return 0
+    } else {
+        document.getElementById('montant').classList.add('bg-danger')
+        return 0
     }
 }
 // verifier le champ de input Discription
@@ -129,22 +144,27 @@ function verifier_Discription() {
     if (get_Discription().length > 0) {
         document.getElementById('discription').classList.remove('bg-danger')
         return 1
-    }else {
-         document.getElementById('discription').classList.add('bg-danger')
-           return 0
+    } else {
+        document.getElementById('discription').classList.add('bg-danger')
+        return 0
     }
 }
 
 //  cree un objet 
 function creer_objet(mantant, description, type) {
     let date = new Date()
+    let autoID = localStorage.getItem("autoID")
+    if (!autoID) {
+        localStorage.setItem("autoID", 1)
+    }
     let objet = {
-        id: 1,
+        id: autoID = localStorage.getItem("autoID"),
         mantant: mantant,
         description: description,
         type: type,
         date: " " + date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate()
     }
+    localStorage.setItem("autoID", ++autoID)
     return objet
 }
 
@@ -211,7 +231,7 @@ function set_MontantDépense(my_key) {
     document.getElementById('MontantSortie').textContent = calculer_montant_Dépense(my_key)
 }
 
-let my_key = "Baztami"
+
 // function update_montants(my_key) {
 set_MontantActual(my_key)
 set_MontantRevenu(my_key)
@@ -235,23 +255,48 @@ for (let index = 0; index < myarray.length; index++) {
     let card = document.createElement('div');
     if (myarray[index].type == "Revenu") {
         card.className = 'card text-white  w-95 m-2  bg-success col   bg-opacity-75';
-        card.innerHTML = ' <div class="row   card-body   fw-bold ">   <div class="col"> +' + myarray[index].mantant + '</div>  <div class="col "> ' + myarray[index].date + '</div><div class="col green-500"> ' + myarray[index].description + '£</div><div class="col-md-1"> <i id="' + myarray[index].id + '"  class="bi bi-pencil-square"></i></div><div class="col-md-1"> <i class="bi bi-trash3-fill"></i></div></div>'
+        card.innerHTML = ' <div class="row   card-body   fw-bold ">   <div class="col"> +' + myarray[index].mantant + '</div>  <div class="col "> ' + myarray[index].date + '</div><div class="col green-500"> ' + myarray[index].description + '£</div><div class="col-md-1"> <i   role="button"  onclick="button_modifier(' + myarray[index].id + ')" class="bi bi-pencil-square"></i></div><div class="col-md-1"> <i class="bi bi-trash3-fill" role="button"  onclick="button_supprimer(' + myarray[index].id + ')"  ></i></div></div>'
     } else {
         card.className = 'card text-white  w-95 m-2 bg-danger col bg-opacity-75';
-        card.innerHTML = ' <div class="row  card-body   fw-bold ">   <div class="col"> -' + myarray[index].mantant + '</div>  <div class="col "> ' + myarray[index].date + '</div><div class="col green-500"> ' + myarray[index].description + ' £</div><div class="col-md-1"> <i class="bi bi-pencil-square"></i></div><div class="col-md-1"> <i class="bi bi-trash3-fill"></i></div></div>'
+        card.innerHTML = ' <div class="row   card-body   fw-bold ">   <div class="col"> +' + myarray[index].mantant + '</div>  <div class="col "> ' + myarray[index].date + '</div><div class="col green-500"> ' + myarray[index].description + '£</div><div class="col-md-1"> <i   role="button"  onclick="button_modifier(' + myarray[index].id + ')" class="bi bi-pencil-square"></i></div><div class="col-md-1"> <i class="bi bi-trash3-fill" role="button"  onclick="button_supprimer(' + myarray[index].id + ')"  ></i></div></div>'
     }
     card_operations.appendChild(card)
 }
 // }
 
-function modifier_card(my_key, id, type, montant, description) {
+
+function button_supprimer(id) {
+    console.log("supp " + id)
+}
+
+
+function button_modifier(id) {
+    console.log("modifier " + id)
     let myarray = charger_les_donnees(my_key)
+    cardTransaction.classList.remove("d-none")
+    btnsave.classList.remove("d-none")
+    btnajout.classList.add('d-none')
+    localStorage.setItem("id_modifier", id)
     for (let index = 0; index < myarray.length; index++)
         if (myarray[index].id == id) {
-            myarray[index].type = type
-            myarray[index].description = description
-            myarray[index].montant = montant
+            set_Type(myarray[index].type)
+            set_Discription(myarray[index].description)
+            set_Montant(parseInt(myarray[index].mantant))
         }
+}
+
+function sauvgarder_modification(my_key) {
+    let myarray = charger_les_donnees(my_key)
+    let id = localStorage.getItem("id_modifier")
+    for (let index = 0; index < myarray.length; index++)
+        if (myarray[index].id == id) {
+            myarray[index].type = get_Type()
+            myarray[index].description = get_Discription()
+            myarray[index].montant = get_Montant()
+        }
+        console.log(myarray)
+    let array_to_string = JSON.stringify(myarray)
+    localStorage.setItem(my_key, array_to_string)
 }
 
 
